a4e2917dcd5c132de558f4f90f0835e5e51b125e
1Snake001 <streetstylecr3w@gmail.com>
2023-06-05 11:35:34 +0200

    table component was implemented with functional delete and update buttons

--- src/components/UserTable.jsx
+++ src/components/UserTable.jsx
@@ -1,10 +1,31 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
-function UserTable({ users }) {
-  console.log(users);
+function UserTable({
+  users,
+  deletUserHandler,
+  setUser,
+  inputValues,
+  setInputValues,
+}) {
+  const navigate = useNavigate();
+
+  const naviGationHandler = () => {
+    navigate("/new");
+  };
+
+  const updateHandler = (user) => {
+    setUser(user);
+    setInputValues({ name: user.name, email: user.email });
+    navigate(`/${user.id}`);
+  };
+
   return (
     <section className="container">
-      <table className="table table-striped">
+      <button onClick={naviGationHandler} className="btn btn-primary">
+        Create New User
+      </button>
+      <table className="table table-striped table-sm">
         <thead>
           <tr>
             <th>#</th>
@@ -13,19 +34,29 @@ function UserTable({ users }) {
             <th>actions</th>
           </tr>
         </thead>
-        <tbody>
+        {users && <tbody>
           {Object.values(users).map((user) => (
             <tr key={user.id}>
               <td>{user.id}</td>
               <td>{user.name}</td>
               <td>{user.email}</td>
               <td>
-                <button className="btn btn-success">Update</button>
-                <button className="btn btn-danger">Delete</button>
+                <button
+                  onClick={() => updateHandler(user)}
+                  className="btn btn-success btn-sm"
+                >
+                  Update
+                </button>
+                <button
+                  onClick={() => deletUserHandler(user.id)}
+                  className="btn btn-danger btn-sm"
+                >
+                  Delete
+                </button>
               </td>
             </tr>
           ))}
-        </tbody>
+        </tbody>}
       </table>
     </section>
   );
