f43f5be75f8b99d5d03eb924efd6f65209f62162
1Snake001 <streetstylecr3w@gmail.com>
2023-06-05 11:43:31 +0200

    form validation was implemented

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {  useState } from "react";
 import Input from "./Input";
 import { useNavigate } from "react-router-dom";
 import services from "../services/services";
@@ -6,29 +6,98 @@ import services from "../services/services";
 function Form({ type, user, getUserData, inputValues, setInputValues }) {
   const navigate = useNavigate();
 
+  const [isValidForm, setIsValidForm] = useState(false);
+  const [errorMessages, setErrorMessages] = useState({
+    name: "",
+    email: "",
+  });
+
+  const textForErrorMessages = {
+    requiredName: "Please enter real name!",
+    moreThan3: "Please enter real name!",
+    requiredEmail: "Please enter a valid email address!",
+  };
+
+  // validateInputChecker functions
+  const isRealName = (name) => {
+    const nameRegex =
+      /^[A-Za-zÁáÉéÍíÓóÖöŐőÚúÜüŰű\s]*[A-Za-zÁáÉéÍíÓóÖöŐőÚúÜüŰű][A-Za-zÁáÉéÍíÓóÖöŐőÚúÜüŰű\s]*$/;
+    return nameRegex.test(name);
+  };
+
+  const isGreaterThanThree = (name) => {
+    return name.length > 3;
+  };
+
+  const isEmail = (email) => {
+    const emailRegex = new RegExp(/^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/, "gm");
+    return emailRegex.test(email);
+  };
+
+  let validators = {
+    name: {
+      requiredName: isRealName,
+      moreThan3: isGreaterThanThree,
+    },
+    email: {
+      requiredEmail: isEmail,
+    },
+  };
+
+  const validator = (name, value) => {
+    let validator = validators[name];
+    let isValid = true;
+    for (let validFn in validator) {
+      if (!validator[validFn](value)) {
+        setErrorMessages((prev) => ({
+          ...prev,
+          [name]: textForErrorMessages[validFn],
+        }));
+        isValid = false;
+      } else {
+        setErrorMessages({ ...errorMessages, [name]: "" });
+      }
+    }
+    setIsValidForm(isValid);
+  };
+
   const onChangeHandler = (event) => {
     const { name, value } = event.target;
     setInputValues({ ...inputValues, [name]: value });
   };
 
+  const onblurHandler = (event) => {
+    const { name, value } = event.target;
+    validator(name, value);
+  };
+
+  function formValidation() {
+    Object.entries(inputValues).map((value) => validator(value[0], value[1]));
+  }
+
   const submitHandler = async (event) => {
     event.preventDefault();
+    formValidation();
 
-    if (type === "update") {
-      const newUserData = {
-        id: user.id,
-        name: inputValues.name,
-        email: inputValues.email,
-      };
-      await services.updateData(newUserData, user.id);
+    if (isValidForm) {
+      if (type === "update") {
+        const newUserData = {
+          id: user.id,
+          name: inputValues.name,
+          email: inputValues.email,
+        };
+        await services.updateData(newUserData, newUserData.id);
+      } else {
+        await services.postData(inputValues);
+      }
+      setInputValues({
+        name: "",
+        email: "",
+      });
+      getUserData();
     } else {
-      await services.postData(inputValues);
+      return;
     }
-    setInputValues({
-      name: "",
-      email: "",
-    });
-    getUserData();
   };
 
   const naviGationHandler = () => {
@@ -46,11 +115,15 @@ function Form({ type, user, getUserData, inputValues, setInputValues }) {
       </button>
       <form onSubmit={submitHandler} action="#">
         <Input
+          errorMessage={errorMessages.name}
           onChange={onChangeHandler}
+          onBlur={onblurHandler}
           name="name"
           inputValue={inputValues.name}
         />
         <Input
+          errorMessage={errorMessages.email}
+          onBlur={onblurHandler}
           onChange={onChangeHandler}
           name="email"
           inputValue={inputValues.email}
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 
-function Input({ name, inputValue, onChange }) {
+function Input({ name, inputValue, onChange, onBlur, errorMessage }) {
   return (
     <div>
       <label htmlFor={name} className="form-label">
         {name}
       </label>
       <input
+        onBlur={onBlur}
         name={name}
         id={name}
         value={inputValue}
         onChange={onChange}
-        className="form-control"
+        className={`form-control ${errorMessage && 'error'}`}
         type="text"
       />
+      <div className="errorMessage text-danger">{errorMessage}</div>
     </div>
   );
 }
