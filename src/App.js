67920fa5075089695400973d3a4d3b57d49f1242
1Snake001 <streetstylecr3w@gmail.com>
2023-06-05 11:47:27 +0200

    router has been added to the App component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,77 @@
 import "./style/App.scss";
 import services from "./services/services";
 import { useEffect, useState } from "react";
-
+import UserTable from "./components/UserTable";
+import NewUserForm from "./components/NewUserForm";
+import UpdateForm from "./components/UpdateForm";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
-   const [users, setUsers] = useState([]);
+  const [inputValues, setInputValues] = useState({
+    name: "",
+    email: "",
+  });
+  const [users, setUsers] = useState([]);
+  const [user, setUser] = useState({});
 
-  let response = services.getData();
-  useEffect(()=> {
+  async function getUserData() {
+    let response = services.getData();
     response.then((usersData) => {
-      setUsers(usersData)
+      setUsers(usersData);
     });
-  },[])
+  }
 
-  return <div className="App"></div>;
+  useEffect(() => {
+    getUserData();
+  }, []);
+
+  const deletUserHandler = async (id) => {
+    await services.deleteData(id);
+    getUserData();
+  };
+
+
+  return (
+    <main className="App">
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <UserTable
+                users={users}
+                deletUserHandler={deletUserHandler}
+                setUser={setUser}
+                inputValues={inputValues}
+                setInputValues={setInputValues}
+              />
+            }
+          />
+          <Route
+            path="/new"
+            element={
+              <NewUserForm
+                getUserData={getUserData}
+                inputValues={inputValues}
+                setInputValues={setInputValues}
+              />
+            }
+          />
+          <Route
+            path={`/${user.id}`}
+            element={
+              <UpdateForm
+                user={user}
+                setUser={setUser}
+                getUserData={getUserData}
+                inputValues={inputValues}
+                setInputValues={setInputValues}
+              />
+            }
+          />
+        </Routes>
+      </Router>
+    </main>
+  );
 }
 
 export default App;
